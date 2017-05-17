7d3dc4c872b87d3ebb02ab88c53fe0ab79616a75
Daniel Fiuza <daniel.fiuza@hotmail.com>
2017-05-17 07:21:57 -0300

    wordfinder with gui

--- WordFinder/Application.py
+++ WordFinder/Application.py
@@ -3,9 +3,11 @@ import tkinter as tk
 import os
 from tkinter.filedialog import askopenfilename
 from tkinter import StringVar
+from tkinter import Listbox
 from tkinter.filedialog import askdirectory
 import textract
 from sys import exit
+
 
 # search for files
 # ftypes = [('all files', '.*')]
@@ -23,78 +25,91 @@ from sys import exit
 # print(root.folderName)
 
 class Application(tk.Frame):
+    # constructor
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.word = tk.StringVar()
+        self.directory_path = tk.StringVar()
+        self.lb_directory_path = tk.Label(self)
+        self.pack()
+        self.create_widgets()
 
-	# constructor
-	def __init__(self,master=None):
-		super().__init__(master)
-		self.word = tk.StringVar()
-		self.directory_path = tk.StringVar()
-		self.lb_directory_path = tk.Label(self)
-		self.pack()
-		self.create_widgets()
+    # create app compoents
+    def create_widgets(self):
+        # Search Folder Button
+        self.lb_word = tk.Label(self, text="Informe a palavra que deseja pesquisar: ")
+        self.lb_word.grid(row=0, column=0,padx=0,columnspan=1,ipady=10,sticky='W')
 
-	# create app compoents
-	def create_widgets(self):
-		# Search Folder Button
-		self.lb_word    = tk.Label(self, text="Palavra que quer pesquisar: ").grid(row=0)
-		self.input_word = tk.Entry(self,textvariable=self.word).grid(row=0, column=1)
-		self.btn_directory_path = tk.Button(
-			self,text="Procupar Pasta",
-			command=self.get_directory,
-		).grid(row=1)
-		self.lb_directory_path.grid(row=1,column=1)
+        self.input_word = tk.Entry(self, textvariable=self.word)
+        self.input_word.grid(row=1, column=0,sticky='W',ipady=10,ipadx=120)
 
-		self.btn_search_files = tk.Button(
-			self,text="Pesquisar",
-			command=self.search_files,
-		).grid(row=2)
+        self.btn_directory_path = tk.Button(self, text="Informe o Diretório",command=self.get_directory,)
+        self.btn_directory_path.grid(row=2,column=0,sticky='W')
+        self.lb_directory_path.grid(row=2, column=0,sticky='E')
 
-	#find files
-	def find_files(self,word,file_path):
-		try:
-			text = textract.process(file_path)
-			textEncoded = text.decode('utf-8')
-			hasTheWord = textEncoded.find(word)
-			if(hasTheWord != -1) :
-				print(file_path)
-				return file_path
-		except:
-			pass
+        self.btn_search_files = tk.Button(self, text="Pesquisar",command=self.search_files,)
+        self.btn_search_files.grid(row=3,ipady=10,ipadx=60,pady=20)
 
-	#list all the directories in given path
-	def list_files(self,startpath,word):
-		files = []
-		for root, dirs, files in os.walk(startpath):
-			level = root.replace(startpath, '').count(os.sep)
-			indent = ' ' * 4 * (level)
-			dir = '{}{}/'.format(indent, os.path.basename(root))
-			# print(dir)
-			subindent = ' ' * 4 * (level + 1)
-			for f in files:
-				files = self.find_files(word,root+'/'+'{}{}'.format(subindent, f).strip())
-				# print('{}{}'.format(subindent, f))
-		return files
+        self.lb_word = tk.Label(self, text="Arquivos Encontrados: ")
+        self.lb_word.grid(row=4,ipady=10)
+        self.scroll_bar = tk.Scrollbar(self)
+        self.scroll_bar.grid(row=5,column=0)
+        self.list_box   = tk.Listbox(self,width=50, height=20)
+        self.list_box.grid(row=5,column=0,ipady=20)
+        # attach listbox to scrollbar
+        self.list_box.config(yscrollcommand=self.scroll_bar.set)
+        self.scroll_bar.config(command=self.list_box.yview)
 
-	def search_files(self):
-		startpath = str(self.directory_path.get())
-		word = str(self.word.get())
-		found_files = self.list_files(startpath,word)
-		# print(str(found_files))
+    # find files
+    def find_files(self, word, file_path):
+        try:
+            text = textract.process(file_path)
+            textEncoded = text.decode('utf-8')
+            hasTheWord = textEncoded.find(word)
+            if (hasTheWord != -1):
+                self.list_box.insert('end', file_path)
+                print(file_path)
+                # return file_path
+        except:
+            pass
 
-	# search and select diretory
-	def get_directory(self):
-		dir_opt = {}
-		dir_opt['initialdir'] = os.environ["HOME"] + '\\'
-		dir_opt['mustexist'] = False
-		dir_opt['title'] = 'Please select directory'
-		self.directory_path.set(askdirectory(**dir_opt))
-		self.lb_directory_path['text'] = self.directory_path.get()
-		print(self.word.get())
-		print(self.directory_path.get())
+    # list all the directories in given path
+    def list_files(self, startpath, word):
+        files_array = []
+        for root, dirs, files in os.walk(startpath):
+            level = root.replace(startpath, '').count(os.sep)
+            indent = ' ' * 4 * (level)
+            dir = '{}{}/'.format(indent, os.path.basename(root))
+            # print(dir)
+            subindent = ' ' * 4 * (level + 1)
+            for f in files:
+                self.find_files(word, root + '/' + '{}{}'.format(subindent, f).strip())
+            # print('{}{}'.format(subindent, f))
+        return files_array
+
+    def search_files(self):
+        startpath = str(self.directory_path.get())
+        word = str(self.word.get())
+        self.list_box.delete(0, 'end')
+        self.list_files(startpath, word)
+        # for file in found_files:
+        #     self.list_box.insert('end', file)
+    # print(str(found_files))
+
+    # search and select diretory
+    def get_directory(self):
+        dir_opt = {}
+        dir_opt['initialdir'] = os.environ["HOME"] + '\\'
+        dir_opt['mustexist'] = False
+        dir_opt['title'] = 'Please select directory'
+        self.directory_path.set(askdirectory(**dir_opt))
+        self.lb_directory_path['text'] = self.directory_path.get()
+        print(self.word.get())
+        print(self.directory_path.get())
 
 
 root = tk.Tk()
 root.title("Word Finder")
-root.geometry("650x400")
+root.geometry("450x600")
 app = Application(master=root)
 app.mainloop()
---
+++ helloWorld2.py
@@ -0,0 +1,11 @@
+# nome  = 'Daniel Fiuza'
+# idade = 24
+#
+# nome = input("Informe seu nome:\n")
+# idade = int(input("Informe sua idade:\n"))
+#
+# print("%s Tem %d anos e vai dormir agora"%(nome,idade))
+
+dicionario = {'nome':'Daniel Fiuza','idade':24,'signo':'ARIES','tipo_sangue':'O'}
+palavras   = ['CACHORRO','ARIRANHA','BALEIA','RATO']   #ordena
+print(sorted(palavras))
