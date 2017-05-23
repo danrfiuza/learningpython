2ab7fbbda0b30ec60b9420024cad6a600e9439c4
danrfiuza <danielfiuza01@gmail.com>
2017-05-23 17:44:27 -0300

    games

--- taggame.py
+++ taggame.py
@@ -1,15 +1,60 @@
 # INTIALISATION
 import pygame,math,sys # import libraries
 from pygame.locals import *
-screen = pygame.display.set_mode((800,600)) #start display
+HEIGHT = 320
+WIDTH  = 240
+screen = pygame.display.set_mode((HEIGHT,WIDTH)) #start display
 character = pygame.image.load('character.png') #load sprite
+enemy     = pygame.image.load('character.png')
 clock = pygame.time.Clock()
 FRAMES_PER_SECOND = 30
 x = y = 200
 speed = direction = 10
 position = (x,y)
-BLACK = (0,0,0)
+BLACK = (0,0,0) # background color
 
+
+#CHARACTER CLASS
+class CharacterSprite(pygame.sprite.Sprite):
+	def  __init__(self,image,x,y,speed):
+		pygame.sprite.Sprite.__init__(self)
+		self.src_image = pygame.image.load(image)
+		self.x = x
+		self.y = y
+		self.speed = speed
+
+	def update(self,x,y):
+		self.x = x
+		self.y = y
+		screen.blit(self.src_image,(x,y))
+
+	def get_x():
+		return self.x
+
+	def get_y():
+		return self.y
+
+#ENEMY CLASS
+class EnemySprite(pygame.sprite.Sprite):
+	def  __init__(self,image,x,y,speed):
+		pygame.sprite.Sprite.__init__(self)
+		self.src_image = pygame.image.load(image)
+		self.x = x
+		self.y = y
+		self.speed = speed
+
+	def draw(self,x,y):
+		screen.blit(self.src_image,(self.x,self.y))
+
+	def pursuit(self,CharacterSprite):
+		if CharacterSprite.x > self.x : self.x += self.speed
+		if CharacterSprite.x < self.x : self.x -= self.speed
+		if CharacterSprite.y > self.y : self.y += self.speed
+		if CharacterSprite.y < self.y : self.y -= self.speed
+
+
+character = CharacterSprite('character.png',200,200,10)
+enemy = EnemySprite('character.png',100,100,10)
 #main loop
 while 1:
 	# USER INPUT
@@ -19,26 +64,26 @@ while 1:
 		down = event.type == KEYDOWN
 		if   event.key == K_RIGHT:
 			x += speed
+			enemy.pursuit(character)
 			print('RIGHT')
 		elif event.key == K_LEFT:
 			x -= speed
+			enemy.pursuit(character)
 			print('LEFT')
 		elif event.key == K_UP:
 			y -= speed
+			enemy.pursuit(character)
 			print('UP')
 		elif event.key == K_DOWN:
 			y += speed
+			enemy.pursuit(character)
 			print('DOWN')
 		elif event.key == K_ESCAPE: sys.exit(0) #quit game
 	screen.fill(BLACK)
-	position = (x , y)
-
+	position = (x,y)
+	print(position)
+	#RENDER AND DRAW
+	character.update(x,y)
+	enemy.draw(100,100)
 	#RENDERING
-	#..rotate the character image for direction
-	# rotated = pygame.transform.rotate(character, direction)
-	#.. position the character  on screen
-	# rect = rotated.get_rect()
-	# rect.center = position
-	# .. render the character to screen
-	screen.blit(character,position)
 	pygame.display.flip()
